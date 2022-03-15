d92b34c38b75d8435a00885b5c4bf87d6c562ac0
kukwa clovis <kukwaclovisngong3@gmail.com>
2022-03-15 15:39:44 +0100

    added fontawesome-6, coded the homeView UI, removed the vue logo

--- client/src/main.js
+++ client/src/main.js
@@ -1,5 +1,11 @@
-import { createApp } from 'vue'
+import {
+     createApp
+} from 'vue'
 import App from './App.vue'
 import router from './router'
+
+
+
+const app = createApp(App);
 
 createApp(App).use(router).mount('#app')
